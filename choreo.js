e874e124020940879f0a906fb666b5c0e0a020e8
Arnish <arnish.shakil@gmail.com>
2024-03-19 00:10:47 -0700

    while loop to create divs

--- choreo.js
+++ choreo.js
@@ -2,3 +2,15 @@
 - make HTML elements, specifically DIVs by using javascript
 
 */
+let i = 0; //initialize the variable i for our loop
+
+const flexContainer = document.getElementById("flexContainer"); //this grabs the id "flex-container" from my HTML document so we can use and reference it in our javascript code
+ 
+
+while (i < 16) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("container");
+    flexContainer.appendchild(newDiv);
+    console.log("added"); //testing
+    i++;
+}
