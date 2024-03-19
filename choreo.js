989f5f8bbe8ab39476dc35e7723169d9cc77cf64
Arnish <arnish.shakil@gmail.com>
2024-03-19 03:02:30 -0700

    added nested loop,css Added the nested loops to get both rows and columns for the grid created HTML divs using .createElement and appended them got each HTML element's ID and to reference and edit them in the next loop added intervals to each mainContainer to diffrentiate them from the previous one, had an issue where all the new containers were beign created under the original one. The intervals helped separate them!

--- choreo.js
+++ choreo.js
@@ -3,14 +3,40 @@
 
 */
 let i = 0; //initialize the variable i for our loop
+let m = 0;
 
-const flexContainer = document.getElementById("flexContainer"); //this grabs the id "flex-container" from my HTML document so we can use and reference it in our javascript code
- 
+//const flexContainer = document.getElementById("mainContainer"); //this grabs the id "flex-container" from my HTML document so we can use and reference it in our javascript code
+const btnAdd = document.querySelector(".btnAdd");
 
-while (i < 16) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("container");
-    flexContainer.appendchild(newDiv);
-    console.log("added"); //testing
+btnAdd.addEventListener("click", containers);
+
+function containers() {
+    while (i<16) {
+        m = 0; // resets M to zero everytime a new mainDiv is created
+
+        const mainDiv = document.createElement("div");
+        mainDiv.id = "mainContainer" + [i]; //adds an ID instead of a class, added an interval here so it can differentiate the new containers from the old
+        document.body.append(mainDiv);
+        const flexContainer = document.getElementById("mainContainer" + [i]); //dont forget to include the interval here as well!!
+        flexContainer.style.display ="flex"; // doing it here because you would have to do it 16+ times with each numerical interval per invrement to get all the mainContainer's taken care of!
+        console.log (flexContainer);
+        console.log("main" + i);
+        while (m < 16) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("container");
+            flexContainer.append(newDiv); //appendChild didnt work because its jquery apparently
+            console.log("added" + m); //testing
+            m++;
+        }
     i++;
+    }    
 }
+
+
+/* Problems faced:
+current problem
+ - all of the containers keep falling under the very first mainContainer instead of the newer ones being created.
+ + solution might be to add an interval after the name of the mainContainer as it's being created
+ ++ another solution here is to get rid of the nested while loop and just use flex-wrap as it does what im trying to achieve with this...
+
+*/
